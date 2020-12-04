f6419fa9a2dc2727159c315f24032ec4a5c905d1
Maneren <49210777+Maneren@users.noreply.github.com>
2020-12-04 21:52:22 +0100

    bakalari

--- src/widgets/Bakalari/Bakalari.js
+++ src/widgets/Bakalari/Bakalari.js
@@ -67,36 +67,42 @@ class Bakalari extends WidgetTemplate {
 
         const lessons = day.Atoms.map(lesson => {
           const changed = lesson.Change !== null;
+          const index = parseInt(data.Hours.find(e => e.Id === lesson.HourId).Caption);
           if (changed) {
             const change = lesson.Change;
 
             switch (change.ChangeType) {
               case 'Removed':
               case 'Canceled':
-                return { changed, changeType: change.ChangeType, changeDescription: change.Description };
+                return {
+                  changed,
+                  blankLesson: true,
+                  index,
+                  changeType: change.ChangeType,
+                  changeDescription: change.Description
+                };
               default:
                 break;
             }
           }
           const subject = data.Subjects.find(e => e.Id === lesson.SubjectId).Abbrev;
-          const time = parseInt(data.Hours.find(e => e.Id === lesson.HourId).Caption);
           const teacher = data.Teachers.find(e => e.Id === lesson.TeacherId).Abbrev;
           const room = data.Rooms.find(e => e.Id === lesson.RoomId).Abbrev;
           // const group = data.Groups.find(e => e.Id === lesson.GroupId).Abbrev;
 
           return {
             subject,
-            time,
+            index,
             teacher,
             room,
-            // group,
             changed
           };
         });
 
         return {
           weekday,
-          lessons
+          lessons,
+          lessonsCount: lessons.length
         };
       }
     );
@@ -105,7 +111,7 @@ class Bakalari extends WidgetTemplate {
       const lessons = timetable[i].lessons;
       for (let j = 0; j < lessons.length; j++) {
         const lesson = lessons[j];
-        if (lesson.time !== j + 1) {
+        if (lesson.index !== j + 1) {
           lessons.splice(j, 0, {});
           break;
         }
@@ -118,13 +124,15 @@ class Bakalari extends WidgetTemplate {
     const toFixedLengthArr = (arr, len) => [...arr, ...(new Array(len - arr.length).fill({}))];
     timetable.map(day => { day.lessons = toFixedLengthArr(day.lessons, maxLength); });
 
+    timetable.maxDayLength = maxLength;
+
     console.log(timetable);
     return timetable;
   }
 
   async updateState () {
     const endpoint = User.endpoints.ACTUAL_TIMETABLE;
-    const json = await this.state.user.getData(endpoint, { date: '2020-09-18' });
+    const json = await this.state.user.getData(endpoint /* { date: '2020-09-18' } */);
     console.log(json);
     const data = Bakalari.processData(json, endpoint);
     this.setState({ timetable: data, loaded: true });
@@ -144,18 +152,29 @@ class Bakalari extends WidgetTemplate {
     if (this.missingCredentials) return (<div className='bakalari-container'>Missing credentials</div>);
     if (!this.state.loaded) return <div className='bakalari-container'><Loader color='#eee' /></div>;
 
+    const timetable = this.state.timetable;
     return (
       <div className='bakalari-container'>
         <table className='timetable'>
+          <thead>
+            <tr>
+              <th key={0} />
+              {new Array(timetable.maxDayLength).fill(0).map((x, i) => <th key={i}>{i + 1}</th>)}
+            </tr>
+          </thead>
           <tbody>
-            {this.state.timetable.map(
+            {timetable.map(
               (day, i) => {
                 return (
                   <tr key={i} className='day'>
                     <td className='weekday'>{day.weekday}</td>
                     {day.lessons.map(
                       (lesson, i) =>
-                        lesson ? (<td key={i} className='lesson'>{lesson.subject}</td>) : (<td className='lesson' />)
+                        lesson
+                          ? (!lesson.blankLesson
+                            ? (<td key={i} className={`lesson ${lesson.changed ? 'changed' : ''}`}>{lesson.subject}</td>)
+                            : <td key={i} className={`lesson ${lesson.changed ? 'changed' : ''}`} />
+                          ) : (<td key={i} className='lesson' />)
                     )}
                   </tr>
                 );
