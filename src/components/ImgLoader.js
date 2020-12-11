87af105f0c0d815231fff2b340acdb0839f74223
Maneren <49210777+Maneren@users.noreply.github.com>
2020-12-11 14:10:37 +0100

    imgLoader component

---
+++ src/components/ImgLoader.js
@@ -0,0 +1,95 @@
+import React from 'react';
+
+import Loader from './Loader';
+
+import './styles/ImgLoader.css';
+
+const Status = {
+  PENDING: 'pending',
+  LOADING: 'loading',
+  LOADED: 'loaded'
+};
+
+export default class ImgLoader extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      src: props.src,
+      status: props.src ? Status.LOADING : Status.PENDING,
+      progress: 0
+    };
+  }
+
+  componentDidMount () {
+    if (this.state.status === Status.LOADING) this.resolveImage();
+  }
+
+  componentDidUpdate () {
+    if (this.props.src !== this.state.src) {
+      this.setState({
+        src: this.props.src,
+        status: this.props.src ? Status.LOADING : Status.PENDING
+      }, () => this.resolveImage());
+    }
+  }
+
+  onError (event) {
+    console.log('Error :', event);
+  }
+
+  onProgress (event) {
+    const progress = parseInt(event.loaded / event.total * 100);
+    this.setState({
+      progress: progress
+    });
+  }
+
+  onLoad (blob) {
+    this.props.OnLoad();
+    this.setState({
+      blob: window.URL.createObjectURL(blob),
+      status: Status.LOADED
+    });
+  }
+
+  componentWillUnmount () {
+    if (this.xmlHTTP) this.xmlHTTP.abort();
+  }
+
+  resolveImage () {
+    // Abort prev request if active
+    if (this.xmlHTTP) this.xmlHTTP.abort();
+
+    this.xmlHTTP = new window.XMLHttpRequest();
+
+    this.xmlHTTP.open('GET', this.props.src, true);
+    this.xmlHTTP.responseType = 'arraybuffer';
+
+    this.xmlHTTP.onerror = event => this.onError(event);
+    this.xmlHTTP.onprogress = event => this.onProgress(event);
+    this.xmlHTTP.onload = event => this.onLoad(new window.Blob([this.xmlHTTP.response]));
+
+    this.xmlHTTP.send();
+  }
+
+  render () {
+    if (this.state.status === Status.LOADING) {
+      return (
+        <>
+          <Loader color='#eee' />
+          {this.state.progress ? <progress max='100' value={this.state.progress} /> : null}
+        </>
+      );
+    }
+
+    if (this.state.status === Status.LOADED) {
+      return <img src={this.state.blob} className={this.props.className} alt={this.props.alt} />;
+    }
+
+    if (this.state.status === Status.PENDING) {
+      return <p>Pending...</p>;
+    }
+
+    return <p>Loading...</p>;
+  }
+}
