15681cc8dac2b2afca529190a9c6b7fe52f1abd5
bouda890 <bouda8900@gmail.com>
2020-12-15 22:24:16 +0100

    reddit js ikons fix

--- src/widgets/Reddit/Reddit.js
+++ src/widgets/Reddit/Reddit.js
@@ -12,7 +12,7 @@ const { requestWithProxy } = Utils.Network;
 const { shuffle } = Utils.Array;
 
 class Reddit extends WidgetTemplate {
-  constructor (props) {
+  constructor(props) {
     super(props);
     this.defaults = {
       loadImages: true,
@@ -27,19 +27,19 @@ class Reddit extends WidgetTemplate {
     };
   }
 
-  get config () {
+  get config() {
     return {
       ...this.defaults,
       ...this.state.config
     };
   }
 
-  componentDidMount () {
+  componentDidMount() {
     super.componentDidMount();
     this.updateState();
   }
 
-  changeImg () {
+  changeImg() {
     const currentIndex = this.state.activePostIndex;
     const loadedPosts = this.state.posts.length;
 
@@ -52,11 +52,11 @@ class Reddit extends WidgetTemplate {
     });
   }
 
-  isBadImgFormat (src) {
+  isBadImgFormat(src) {
     return (src.search(/(\.gifv)/) !== -1) || (src.search(/(v\.redd)/) !== -1);
   }
 
-  updateState () {
+  updateState() {
     console.log('UPDATE');
     const fetchLimit = Math.ceil(this.config.preloadPosts / this.config.subreddits.length);
     const requests = this.config.subreddits.map(subreddit => {
@@ -84,7 +84,7 @@ class Reddit extends WidgetTemplate {
     });
   }
 
-  processData (data) {
+  processData(data) {
     data = JSON.parse(data);
     // console.log(data);
     return data.data.children.map((post, i) => {
@@ -113,20 +113,20 @@ class Reddit extends WidgetTemplate {
     });
   }
 
-  componentWillUnmount () {
+  componentWillUnmount() {
     clearTimeout(this.updateClock);
   }
 
-  get activePost () {
+  get activePost() {
     return this.state.posts[this.state.activePostIndex];
   }
 
-  imgLoaded () {
+  imgLoaded() {
     clearTimeout(this.updateClock);
     this.updateClock = setTimeout(this.changeImg.bind(this), this.config.imgChangeInterval * 1000);
   }
 
-  render () {
+  render() {
     if (!this.state.loaded) return <div className='reddit-container'><Loader color='#eee' /></div>;
     const activePost = this.activePost;
     return (
@@ -139,8 +139,47 @@ class Reddit extends WidgetTemplate {
         <div className='title'>{activePost.title}</div>
         <ImgLoader src={activePost.src} className='img' alt='post img' OnLoad={this.imgLoaded.bind(this)} proxy='http://127.0.0.1:3100' />
         <div>
-          <span className='score'><i className='gg-arrow-up-r' />{activePost.score}</span>
-          <span className='comments'><i className='gg-comment' />{activePost.comments}</span>
+          <span className='score'>
+            <svg
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M14.8543 11.9741L16.2686 10.5599L12.0259 6.31724L7.78327 10.5599L9.19749 11.9741L11.0259 10.1457V17.6828H13.0259V10.1457L14.8543 11.9741Z"
+                fill="currentColor"
+              />
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M1 19C1 21.2091 2.79086 23 5 23H19C21.2091 23 23 21.2091 23 19V5C23 2.79086 21.2091 1 19 1H5C2.79086 1 1 2.79086 1 5V19ZM5 21H19C20.1046 21 21 20.1046 21 19V5C21 3.89543 20.1046 3 19 3H5C3.89543 3 3 3.89543 3 5V19C3 20.1046 3.89543 21 5 21Z"
+                fill="currentColor"
+              />
+            </svg>
+
+            <span>{activePost.score}</span>
+          </span>
+          <span className='comments'>
+            <svg
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M17 9H7V7H17V9Z" fill="currentColor" />
+              <path d="M7 13H17V11H7V13Z" fill="currentColor" />
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M2 18V2H22V18H16V22H14C11.7909 22 10 20.2091 10 18H2ZM12 16V18C12 19.1046 12.8954 20 14 20V16H20V4H4V16H12Z"
+                fill="currentColor"
+              />
+            </svg>
+            <span>{activePost.comments}</span>
+          </span>
         </div>
       </div>
     );
