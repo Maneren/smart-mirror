d36d6d80745a1f92930f5b5c99502f233a922eb2
Maneren <49210777+Maneren@users.noreply.github.com>
2021-01-24 20:52:53 +0100

    sensor developement

--- src/App.js
+++ src/App.js
@@ -64,12 +64,14 @@ class App extends Component {
     const { spawn } = await window.require('child_process');
 
     // this.exe = spawn('node', ['-i', './src/gestureSensor/print.js']);
-    this.exe = spawn('sudo', ['./PAJ7620U2']);
-    this.exe.stdout.on('data', data => this.handleSensorInput(data));
+    // this.exe = spawn('sudo', ['./PAJ7620U2']);
+    this.exe = spawn('pwd');
+    // this.exe.stdout.on('data', data => this.handleSensorInput(data));
+    this.exe.stdout.on('data', data => console.log(`stdout: ${data}`));
     this.exe.stderr.on('data', data => console.log(`stderr: ${data}`));
     this.exe.on('close', exitCode => {
       console.error('Sensor listener exited: ' + exitCode);
-      this.initGestureSensor();
+      // this.initGestureSensor();
     });
     console.log(this.exe);
   }
